fca8b4ca81e3fdc3fc5995be37ff6197cdf18f21
lmn0212 <l.m.nykytiuk@gmail.com>
2017-04-10 20:04:25 +0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,35 +1,48 @@
 <!DOCTYPE html>
+<html lang="ua">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Калькулятор валют</title>
-<link href="/demo/css/simple.css" rel="stylesheet" type="text/css" />
-<link href="calc.css" rel="stylesheet" type="text/css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="calc.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Калькулятор валют</title>
 </head>
 
 <body>
 
 <div id="content">
-    <h1>Калькулятор валют для сайта - Демо</h1>
-        <div class="calc_body">
-        <span>Калькулятор стоимости</span>
-        <form name="calc_form" action="" id="calc_formid">
-            <label for="calc_amount">Cумма в валюті:</label>
-            <input name="calc_amount" value="1" size="4" />
-            <div class="calc_clear"></div>
-            <label for="calc_rates">Валюта:</label>
-            <select name="calc_rates" id="ratesoption">
 
-            </select>
-            <div class="calc_clear"></div>
-            <input type="submit" value="Розрахунок в грн." name="calc_do" class="calc_button" />
+    <div class="calc_body">
+        <span>Калькулятор</span>
+        <form name="calc_form" action="" id="calc_formid" class="col-sm-6">
+            <div class="form-group col-sm-6">
+                <span class="input-group-addon" id="calc_amount">Cумма в валюті:</span>
+                <input type="number" name="calc_amount" class="form-control" aria-describedby="calc_amount">
+            </div>
+
+            <div class="form-group col-sm-6">
+                <label for="ratesoption">Валюта:</label>
+                <select name="calc_rates" id="ratesoption" class="form-control">
+                </select>
+            </div>
+        <div class="calc_clear" style="clear: both;"></div>
+
+                <button type="submit" name="calc_do" class="btn btn-lg btn-primary calc_button col-sm-6" style="margin-top: 2%">Розрахунок в грн.</button>
+                <div class="form-group col-sm-6">
+                    <label for="calc_result">Результат: </label>
+                    <input type="number" name="result" id="calc_result" class="form-control" value="" readonly>
+                </div>
+
         </form>
-        <label>Результат: </label><input name="result" id="calc_result" value="" disabled="disabled" size="4" />
+
         <div class="calc_clear"></div>
         <div id="calc_error"></div>
-        </div>
+    </div>
+    <div class="input-group">
 
+    </div>
+    
 </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="calc.js"></script>
 </body>
 </html>
